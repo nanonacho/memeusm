a61cddae80f553d070c4566a2c32d35eaa6bea20
nose103 <nose10@hotmail.cl>
2020-12-02 20:57:13 -0300

    Actualizacion de interfaz 2
    Se subiran los archivos actualizados y las estructuras de archivos ordenada

---
+++ webpage/js/login.js
@@ -0,0 +1,25 @@
+function authState() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            alert("Te has logueado!")
+            window.location.replace("index.html")
+        } else {
+            alert("Has salido de tu cuenta!")
+        }
+      });
+}
+function login() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    firebase.auth().signInWithEmailAndPassword(email.value, password.value).then(() => {
+        authState()
+      }).catch(e => alert(e.message))
+}
+
+function logout() {
+    firebase.auth().signOut();
+}
+
+
+
+
---
+++ webpage/js/register.js
@@ -0,0 +1,31 @@
+function authState() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            alert("Te has logueado!")
+            window.location.replace("index.html")
+        } else {
+            alert("Has salido de tu cuenta!")
+        }
+      });
+}
+
+
+function register(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    if (email.value.includes("@usm.cl") ||  email.value.includes("@sansano.usm.cl")){
+        const promise = firebase.auth().createUserWithEmailAndPassword(email.value, password.value);
+        promise.catch(e => alert(e.message));
+    }
+    else{
+        alert("¡Solo se permiten correos @sansano.usm.cl y @usm.cl!")
+    }
+};
+
+function logout() {
+    firebase.auth().signOut();
+}
+
+
+
+
