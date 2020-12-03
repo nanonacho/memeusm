1c7a0bb01cd95ef944e4c5ec2204fc65a4a7fd3f
nanonacho <71726968+nanonacho@users.noreply.github.com>
2020-12-03 00:19:51 -0300

    Modificación de alertas login y register
    Se agregaron alertas en español sobre errores al momento de loguearse o registrarse.

--- webpage/js/login.js
+++ webpage/js/login.js
@@ -13,9 +13,23 @@ function login() {
     var password = document.getElementById("password");
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
     firebase.auth().signInWithEmailAndPassword(email.value, password.value).then(() => {
-        
         authState()
-      }).catch(e => alert(e.message))
+      }).catch(function(error){
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode == "auth/wrong-password") {
+          alert("¡Contraseña equivocada!");
+        }
+        else if (errorCode == "auth/invalid-email") {
+            alert("¡Email inválido!");
+        }
+        else if (errorCode == "auth/user-not-found") {
+            alert("¡Usuario no encontrado!");
+        }
+        else {
+          alert(errorMessage);
+        }
+      })
 }
 
 
--- webpage/js/register.js
+++ webpage/js/register.js
@@ -7,7 +7,22 @@ function register(){
     console.log(password_conf.value)
     if ((email.value.includes("@usm.cl") ||  email.value.includes("@sansano.usm.cl") ) && (password.value == password_conf.value)   ){
         const promise = firebase.auth().createUserWithEmailAndPassword(email.value, password.value);
-        promise.catch(e => alert(e.message));
+        promise.catch(function(error){
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode == "auth/email-already-in-use") {
+              alert("¡Email en uso!");
+            }
+            else if (errorCode == "auth/invalid-email") {
+                alert("¡Email inválido!");
+            }
+            else if (errorCode == "auth/weak-password") {
+                alert("¡Contraseña debil!");
+            }
+            else {
+              alert(errorMessage);
+            }
+          });
     }
     else{
         alert("¡Solo se permiten correos @sansano.usm.cl y @usm.cl y las contraseñas deben ser iguales!")
