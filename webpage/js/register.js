6170c24fd0308953b1ba4a56586e34eb09a8b796
nose103 <nose10@hotmail.cl>
2020-12-11 19:09:44 -0300

    Se agrega overlay de registro exitoso

--- webpage/js/register.js
+++ webpage/js/register.js
@@ -1,17 +1,21 @@
-
+function Redirect() 
+    {  
+        document.getElementById("overlay").style.display = "none";
+        window.location="index.html"; 
+    } 
 
 function register(){
     var email = document.getElementById("email");
     var password = document.getElementById("password");
     var password_conf = document.getElementById("password_conf");
-    console.log(email.value)
-    console.log(password.value)
-    console.log(password_conf.value)
     if ((email.value.includes("@usm.cl") ||  email.value.includes("@sansano.usm.cl") ) && (password.value == password_conf.value)   ){
         const promise = firebase.auth().createUserWithEmailAndPassword(email.value, password.value);
+        document.getElementById("overlay").style.display = "block";
+            setTimeout('Redirect()', 3000);
         promise.catch(function(error){
             var errorCode = error.code;
             var errorMessage = error.message;
+            console.log(errorCode)
             if (errorCode == "auth/email-already-in-use") {
               alert("¡Email en uso!");
             }
@@ -24,8 +28,10 @@ function register(){
             else {
               alert(errorMessage);
             }
-          });
+          }
+          );
     }
+
     else{
         alert("¡Solo se permiten correos @sansano.usm.cl y @usm.cl y las contraseñas deben ser iguales!")
     }
